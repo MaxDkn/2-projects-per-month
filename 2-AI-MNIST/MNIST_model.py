1536bd894e738d872d04137606b7df1497aeaae4
maxde <max.deckmyn@gmail.com>
2025-01-07 19:49:30 +0100

    Fixe arboresence

--- 2-AI-MNIST/MNIST_model.py
+++
@@ -1,9 +0,0 @@
-import os.path
-import pickle
-
-
-if __name__ == '__main__':
-    if os.path.isfile('./model_weight.pkl'):
-        W1, b1, W2, b2 = pickle.load('./model_weight.pkl')
-    else:
-        raise "Fichier non trouvé"
